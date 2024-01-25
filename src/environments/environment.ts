b85eb2fb61f5d0e16d29c0b11d8c8562c2f48ef4
arceecarlo <arceecarlo@gmail.com>
2024-01-25 16:57:21 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  APP_ID: '',
-  GRAPHQL_URI: '',
-  API_KEY: ''
+  production: true,
+  APP_ID: 'bakery-isnzi',
+  GRAPHQL_URI: 'https://asia-south1.gcp.realm.mongodb.com/api/client/v2.0/app/bakery-isnzi/graphql',
+  API_KEY: 'd4hLBX8xuviBx8O8ffsci0eK0komigpPeS8F9EyYqhsjmnWc6rss9Z5qjaoqpQt8'
 };
